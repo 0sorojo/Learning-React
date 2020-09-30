a4b0fa335c5741ee6c567707506f0906084d95eb
0sorojo <redrootproductions@gmail.com>
2020-09-29 23:50:28 -0400

    prevState and setState

---
+++ src/components/EventStarter.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+
+class EventStarter extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+      style: {
+        padding: "5px",
+        color: "black",
+      },
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick = () => {
+    this.setState((prevState) => {
+      return {
+        count: prevState.count + 1,
+      };
+    });
+    console.log(this.state);
+  };
+
+  render() {
+    return (
+      <div>
+        <img
+          onMouseOver={this.handleClick}
+          src="https://media.giphy.com/media/WQaHrd6Z3038DofLRl/giphy.gif"
+        />
+        <br />
+        <br />
+        <div>{this.state.count}</div>
+        <button onClick={this.handleClick} style={this.state.style}>
+          Click Me
+        </button>
+      </div>
+    );
+  }
+}
+
+export default EventStarter;
--- src/components/StateContainer.js
+++ src/components/StateContainer.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import EventStarter from "./EventStarter";
 
 class StateContainer extends Component {
   constructor() {
@@ -11,9 +12,18 @@ class StateContainer extends Component {
 
   render() {
     return (
-      <div>
-        <h1>{this.state.title}</h1>
-        <h3>It's the season of {this.state.season}</h3>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <div>
+          <h1>{this.state.title}</h1>
+          <h3>It's the season of {this.state.season}</h3>
+        </div>
+        <EventStarter />
       </div>
     );
   }
